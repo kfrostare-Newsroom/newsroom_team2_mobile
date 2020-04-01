41a5f23e2a8ebb2ae3d7fbe672a48a50b401bed5
Jaime <jaime.cruz.hostalot@gmail.com>
2020-04-01 16:04:01 +0200

    [Installation] Deleted unnecessary files/folders

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { IonApp,
    IonHeader,
    } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
